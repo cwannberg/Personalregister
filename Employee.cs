83c175cf179ca3eefe3a529794c4b0d4f53cbbb3
Cilla <ceciliawannberg@gmail.com>
2025-05-21 16:58:15 +0200

    Commit

--- Employee.cs
+++ Employee.cs
@@ -1,14 +1,23 @@
-﻿using System.Xml;
-
-namespace Personalregister
+﻿namespace Personalregister
 {
-    public class Employee(string name, string lastName, int salary)
+    public class Employee
     {
-        public string Name { get; } = name;
-        public string LastName { get; } = lastName;
-        public int Salary { get; } = salary;
+        public int Id { get; }
+        public string Name { get; set; }
+        public string LastName { get; set; }
+        public int Salary { get; set; }
+
+        private static int idCounter = 1;
 
         public static List<Employee> ListOfEmployees = new List<Employee>();
+
+        public Employee(string name, string lastName, int salary)
+        {
+            Id = idCounter++;
+            Name = char.ToUpper(name[0]) + name.Substring(1);
+            LastName = char.ToUpper(lastName[0]) + lastName.Substring(1);
+            Salary = salary;
+        }
 
         public static void GetListOfEmployees()
         {
@@ -16,8 +25,9 @@ namespace Personalregister
             {
                 foreach (var employee in ListOfEmployees)
                 {
-                    Console.WriteLine($"Namn: {employee.Name} {employee.LastName} Lön: {employee.Salary}");
+                    Console.WriteLine($"Id: {employee.Id} Namn: {employee.Name} {employee.LastName} Lön: {employee.Salary}");
                 }
+                Console.WriteLine("\n");
             }
             else
             {
@@ -26,18 +36,70 @@ namespace Personalregister
         }
         public static void AddEmployee(string name, string lastName, int salary)
         {
-            name = char.ToUpper(name[0]) + name.Substring(1);
-            lastName = char.ToUpper(lastName[0]) + lastName.Substring(1);
-
             ListOfEmployees.Add(new Employee(name, lastName, salary));
         }
-        private void UpdateEmployee()
+        public static void UpdateEmployee()
         {
+            if(ListOfEmployees.Count <= 0)
+            {
+                Console.WriteLine("Listan är tom");
+                return;
+            }
+            Console.WriteLine("Ange id på personen du vill ändra");
+            if (int.TryParse(Console.ReadLine(), out int idToUpdate))
+            {
+                var employeeToUpdate = ListOfEmployees.FirstOrDefault(e => e.Id == idToUpdate);
+                if (employeeToUpdate != null)
+                {
+                    Console.WriteLine($"Uppdaterar {employeeToUpdate.Name} {employeeToUpdate.LastName}");
 
+                    Console.Write("Nytt förnamn: ");
+                    string newName = Console.ReadLine();
+                    Console.Write("Nytt efternamn: ");
+                    string newLastName = Console.ReadLine();
+                    Console.Write("Ny lön: ");
+                    string salaryInput = Console.ReadLine();
+
+                    if (int.TryParse(salaryInput, out int newSalary))
+                    {
+                        employeeToUpdate.Name = char.ToUpper(newName[0]) + newName.Substring(1);
+                        employeeToUpdate.LastName = char.ToUpper(newLastName[0]) + newLastName.Substring(1);
+                        employeeToUpdate.Salary = newSalary;
+
+                        Console.WriteLine("Anställd uppdaterad!");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Ogiltig lön – uppdatering avbruten.");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Ingen anställd med det id:t hittades.");
+                }
+            }
         }
-        private void RemoveEmployee()
+        public static void RemoveEmployee()
         {
-
+            if (ListOfEmployees.Count <= 0)
+            {
+                Console.WriteLine("Listan är tom");
+                return;
+            }
+            Console.WriteLine("Ange id på personen du vill radera");
+            if (int.TryParse(Console.ReadLine(), out int idToRemove))
+            {
+                var employeeToRemove = ListOfEmployees.FirstOrDefault(e => e.Id == idToRemove);
+                if (employeeToRemove != null)
+                {
+                    ListOfEmployees.Remove(employeeToRemove);
+                    Console.WriteLine("Den anställda är borttagen från listan");
+                }
+                else
+                {
+                    Console.WriteLine("Ingen anställd med det id:t hittades.");
+                }
+            }
         }
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
-﻿namespace Personalregister
+﻿using System.ComponentModel;
+
+namespace Personalregister
 {
     internal class Program
     {
@@ -9,42 +11,54 @@
 
         private static void ProgramMenu()
         {
-            Console.WriteLine("Lexicon personalregister");
-            Console.WriteLine("Välj mellan ett av följande alternativ:");
-            Console.WriteLine("1. Skriv ut registret");
-            Console.WriteLine("2. Lägg till ny anställd");
-            Console.WriteLine("3. Ändra befintlig anställd");
-            Console.WriteLine("4. Ta bort anställd");
-            string usersChoice = Console.ReadLine();
+            while (true) { 
+                Console.WriteLine("Lexicon personalregister");
+                Console.WriteLine("Välj mellan ett av följande alternativ:");
+                Console.WriteLine("1. Skriv ut registret");
+                Console.WriteLine("2. Lägg till ny anställd");
+                Console.WriteLine("3. Ändra befintlig anställd");
+                Console.WriteLine("4. Ta bort anställd");
+                Console.WriteLine("5. Avsluta programmet");
+                string usersChoice = Console.ReadLine();
 
-            switch (usersChoice)
-            {
-                case "1":
-                    Console.WriteLine("\nPersonalregister: ");
-                    Employee.GetListOfEmployees();
-                    break;
-                case "2":
-                    Console.WriteLine("Lägg till förnamn: ");
-                    string name = Console.ReadLine().ToLower();
-                    Console.WriteLine("Lägg till efternamn: ");
-                    string lastName = Console.ReadLine().ToLower();
-                    Console.WriteLine("Lägg till lön: ");
-                    string salaryInput = Console.ReadLine();
-
-                    if(int.TryParse(salaryInput, out int salary))
-                        Employee.AddEmployee(name, lastName, salary);
-                    else
-                        Console.WriteLine("Du har gjort en felaktig inmatning, lönen ska anges i siffror. Försök igen.\n");
-                    break;
-                case "3":
-                    break;
-                case "4":
-                    break;
-                default:
-                    Console.WriteLine("Du har gjort en felaktig inmatning, försök igen.\n");
-                    break;
+                switch (usersChoice)
+                {
+                    case "1":
+                        Console.WriteLine("\nPersonalregister: ");
+                        Employee.GetListOfEmployees();
+                        break;
+                    case "2":
+                        AddEmployeeMenu();
+                        break;
+                    case "3":
+                            Employee.UpdateEmployee();
+                        break;
+                    case "4":
+                            Employee.RemoveEmployee();
+                        break;
+                    case "5":
+                        Console.WriteLine("Tack för att du använde Lexicons personalregister. Välkommen tillbaka!");
+                        return;
+                    default:
+                        Console.WriteLine("Du har gjort en felaktig inmatning, försök igen.\n");
+                        break;
+                }
             }
-            ProgramMenu();
+        }
+
+        private static void AddEmployeeMenu()
+        {
+            Console.WriteLine("Lägg till förnamn: ");
+            string name = Console.ReadLine().ToLower();
+            Console.WriteLine("Lägg till efternamn: ");
+            string lastName = Console.ReadLine().ToLower();
+            Console.WriteLine("Lägg till lön: ");
+            string salaryInput = Console.ReadLine();
+
+            if (int.TryParse(salaryInput, out int salary))
+                Employee.AddEmployee(name, lastName, salary);
+            else
+                Console.WriteLine("Du har gjort en felaktig inmatning, lönen ska anges i siffror. Försök igen.\n");
         }
     }
 }
